ac5645169e2af0a112d0476bf62479307dd627bf
Matheus <matheus2hcosta@gmail.com>
2023-04-19 02:11:37 -0300

    feat: inicialize with terraform

---
+++ terraform/docker/main.tf
@@ -0,0 +1,31 @@
+## Version 3> is failing in docker local
+## Some bug in Windows cant read Dockerfil test: https://github.com/docker/buildx/issues/426#issuecomment-732980948
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+
+provider "docker" {
+    ## TO Linus should be host = "unix:///var/run/docker.sock"
+    #host = "tcp://localhost:2375"
+    host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "daily-novels-api-image" {
+  name = "daily-novels-api-image:latest"
+  build {
+    path = "../.."
+    label = {
+      author : "@escollhas"
+    }
+  }
+}
+
+resource "docker_container" "daily-novels-api-container" {
+  name  = "daily-novels-api-container"
+  image = docker_image.daily-novels-api-image.image_id
+}
---
+++ terraform/docker/variables.tf
@@ -0,0 +1,15 @@
+variable "app_name" {
+  default = "my-fastapi-app"
+}
+
+variable "docker_image_name" {
+  default = "my-fastapi-image"
+}
+
+variable "docker_image_version" {
+  default = "latest"
+}
+
+variable "port" {
+  default = 80
+}
